a4090e562bb0deac307328a6f3be29a628e53ce5
Miguel Ángel MC <mamoca28@hotmail.es>
2024-12-30 19:19:13 +0100

    feat: Primer diseño de la GUI

---
+++ Prácticas/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+# Create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.secret_key = 'GRXViajes'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#from . import db
+#db.init_app(app)
+
+#from . import auth
+#app.register_blueprint(auth.bp)
+
+#from . import blog
+#app.register_blueprint(blog.bp)
+#app.add_url_rule('/', endpoint='index')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
